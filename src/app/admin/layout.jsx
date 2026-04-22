7b715a937552ee0fc5edbf17956e78e82df4ccba
Alfian Andre Ramadhan <andrealfian69699@gmail.com>
2026-04-22 16:08:18 +0700

    feat: remove token validation and redirect logic from AdminLayout

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -3,12 +3,5 @@ import { redirect } from 'next/navigation';
 import ClientLayout from '@/components/layout/admin/ClientLayout';
 
 export default async function AdminLayout({ children }) {
-  const cookiesStore = await cookies();
-  const token = cookiesStore.get('refreshToken')?.value;
-
-  if (!token) {
-    redirect('/login');
-  }
-
   return <ClientLayout>{children}</ClientLayout>;
 }
